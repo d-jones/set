5f06af2f505c1934702cf285b6e24e2d54fa70f2
Dillon Arnold <cyanosnow@gmail.com>
2013-10-28 22:09:59 -0700

    Wrote intitial code.

--- SJProj/src/edu/up/cs301/set/SetCardsAction.java
+++ SJProj/src/edu/up/cs301/set/SetCardsAction.java
@@ -1,27 +1,38 @@
 package edu.up.cs301.set;
 
+import edu.up.cs301.card.Card;
 import edu.up.cs301.game.GamePlayer;
 
 /**
- * A SJPlayAction is an action that represents playing a card on the "up"
- * pile.
+ * A SetCardsAction is an action that represents the three cards
+ * the player has chosen.
  * 
- * @author Steven R. Vegdahl
- * @version 31 July 2002
+ * @author Dillon Arnold
+ * @version 28 October 2013
  */
 public class SetCardsAction extends SetCallAction
 {
 	private static final long serialVersionUID = 3250639793499599047L;
+	
+	private Card[] cards = new Card[3];
 
 	/**
-     * Constructor for the SJPlayMoveAction class.
+     * Constructor for the SetCardsAction class.
      * 
-     * @param source  the player making the move
+     * @param player  the player making the move
+     * @param c1 the first card chosen
+     * @param c2 the second card chosen
+     * @param c3 the third card chosen
      */
-    public SetCardsAction(GamePlayer player)
+    public SetCardsAction(GamePlayer player, Card c1, Card c2, Card c3)
     {
-        // initialize the source with the superclass constructor
+        // Initialize the player with the superclass constructor
         super(player);
+        
+        // Add cards to array
+        cards[0] = c1;
+        cards[1] = c2;
+        cards[2] = c3;
     }
 
     /**
