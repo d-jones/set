c4659b9ba0b6de064e97f229c2e0186090ac8bcb
Dillon Arnold <cyanosnow@gmail.com>
2013-10-28 22:25:01 -0700

    Changed Project Name

---
+++ SjProj/src/edu/up/cs301/set/SetCardsAction.java
@@ -0,0 +1,46 @@
+package edu.up.cs301.set;
+
+import edu.up.cs301.card.Card;
+import edu.up.cs301.game.GamePlayer;
+
+/**
+ * A SetCardsAction is an action that represents the three cards
+ * the player has chosen.
+ * 
+ * @author Dillon Arnold
+ * @version 28 October 2013
+ */
+public class SetCardsAction extends SetCallAction
+{
+	private static final long serialVersionUID = 3250639793499599047L;
+	
+	private Card[] cards = new Card[3];
+
+	/**
+     * Constructor for the SetCardsAction class.
+     * 
+     * @param player  the player making the move
+     * @param c1 the first card chosen
+     * @param c2 the second card chosen
+     * @param c3 the third card chosen
+     */
+    public SetCardsAction(GamePlayer player, Card c1, Card c2, Card c3)
+    {
+        // Initialize the player with the superclass constructor
+        super(player);
+        
+        // Add cards to array
+        cards[0] = c1;
+        cards[1] = c2;
+        cards[2] = c3;
+    }
+
+    /**
+     * @return
+     * 		whether this action is a "play" move
+     */
+    public boolean isPlay() {
+        return true;
+    }
+    
+}
---
+++ SjProj/src/edu/up/cs301/set/SetGame.java
@@ -0,0 +1,13 @@
+package edu.up.cs301.set;
+
+import edu.up.cs301.game.Game;
+
+/**
+ * An interface that implements a Slapjack game
+ * 
+ * @author Steven R. Vegdahl
+ * @version 25 July 2002
+ */
+public interface SetGame extends Game
+{
+}
