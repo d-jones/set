efe3299f8b4771a36a4abe2c695e1dd48025561c
Dillon Arnold <cyanosnow@gmail.com>
2013-10-30 20:48:07 -0700

    First version finished. May need to add method to hide cards.

--- Set/src/edu/up/cs301/set/SetState.java
+++ Set/src/edu/up/cs301/set/SetState.java
@@ -21,12 +21,14 @@ public class SetState extends GameState
     //  - 0: Game deck
     //  - 1: Cards in play
     //  - 2: Discard deck
-	
-    private Deck[] decks;
+	private Deck[] decks;
     
     // Whose turn it is to select a set
     // Set to -1 when it is no player's turn
     private int toPlay;
+    
+    // Score of each player sorted by index
+    private int[] scores;
 
     /**
      * Constructor for objects of class SetState. Initializes for the beginning of the
@@ -34,25 +36,27 @@ public class SetState extends GameState
      *  
      */
     public SetState() {
-   
+    	// Initially no player's turn
     	toPlay = -1;
     	
-    	// initialize the decks as follows:
-    	// - each player deck (#0 and #1) gets half the cards, randomly
-    	//   selected
-    	// - the middle deck (#2) is empty
-    	piles = new Deck[3];
-    	piles[0] = new Deck(); // create empty deck
-    	piles[1] = new Deck(); // create empty deck
-    	piles[2] = new Deck(); // create empty deck
-    	piles[toPlay].add52(); // give all cards to player whose turn it is, in order
-    	piles[toPlay].shuffle(); // shuffle the cards
-    	// move cards to opponent, until to piles have ~same size
-    	while (piles[toPlay].size() >=
-    			piles[1-toPlay].size()+1) {
-    		piles[toPlay].moveTopCardTo(piles[1-toPlay]);
-    	}
+    	// Initialize the decks as follows:
+    	// - Game deck has all 81 cards shuffled
+    	// - Move 12 cards to play deck
+    	// - Discard deck empty
+    	decks = new Deck[3];
+    	decks[0] = new Deck(); // create empty deck
+    	decks[1] = new Deck(); // create empty deck
+    	decks[2] = new Deck(); // create empty deck
+    	decks[0].add81(); // initialize game deck
+    	decks[0].shuffle(); // shuffle the cards
+    	
+    	// Initialize scores to 0
+    	scores[0] = 0;
+    	scores[1] = 0;
+    	scores[2] = 0;
+    	scores[3] = 0;
     }
+    
     
     /**
      * Copy constructor for objects of class SJState. Makes a copy of the given state
@@ -60,61 +64,46 @@ public class SetState extends GameState
      * @param orig  the state to be copied
      */
     public SetState(SetState orig) {
-    	// set index of player whose turn it is
+    	// Copy toPlay
     	toPlay = orig.toPlay;
-    	// create new deck array, making copy of each deck
-    	piles = new Deck[3];
-    	piles[0] = new Deck(orig.piles[0]);
-    	piles[1] = new Deck(orig.piles[1]);
-    	piles[2] = new Deck(orig.piles[2]);
+    	
+    	// Copy decks
+    	decks = new Deck[3];
+    	decks[0] = orig.decks[0]; 
+    	decks[1] = orig.decks[1]; 
+    	decks[2] = orig.decks[2]; 
+ 
+    	// Copy scores
+    	scores[0] = orig.scores[0];
+    	scores[1] = orig.scores[1];
+    	scores[2] = orig.scores[2];
+    	scores[3] = orig.scores[3];
     }
     
     /**
-     * Gives the given deck.
+     * Tells which player's turn it is
      * 
-     * @return  the deck for the given player, or the middle deck if the
-     *   index is 2
-     */
-    public Deck getDeck(int num) {
-        if (num < 0 || num > 2) return null;
-        return piles[num];
-    }
-    
-    /**
-     * Tells which player's turn it is.
-     * 
-     * @return the index (0 or 1) of the player whose turn it is.
+     * @return the index (0-4, -1 if none) of the player whose turn it is.
      */
     public int toPlay() {
         return toPlay;
     }
     
     /**
-     * change whose move it is
+     * Change whose move it is
      * 
-     * @param idx
-     * 		the index of the player whose move it now is
+     * @param idx - the index of the player whose move it now is
      */
     public void setToPlay(int idx) {
     	toPlay = idx;
     }
- 
+    
     /**
-     * Replaces all cards with null, except for the top card of deck 2
+     * Returns the score of the player
+     * 
+     * @return idx - the index of the player
      */
-    public void nullAllButTopOf2() {
-    	// see if the middle deck is empty; remove top card from middle deck
-    	boolean empty2 = piles[2].size() == 0;
-    	Card c = piles[2].removeTopCard();
-    	
-    	// set all cards in deck to null
-    	for (Deck d : piles) {
-    		d.nullifyDeck();
-    	}
-    	
-    	// if middle deck had not been empty, add back the top (non-null) card
-    	if (!empty2) {
-    		piles[2].add(c);
-    	}
+    public int getScore(int idx) {
+        return scores[idx];
     }
 }
