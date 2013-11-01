78a9c64df205d4b5098d5451e38d9f951c768ce9
Dillon Arnold <cyanosnow@gmail.com>
2013-11-01 11:37:28 -0700

    Added getCard()

--- Set/src/edu/up/cs301/set/SetCardsAction.java
+++ Set/src/edu/up/cs301/set/SetCardsAction.java
@@ -13,39 +13,46 @@ import edu.up.cs301.game.GamePlayer;
 public class SetCardsAction extends SetCallAction
 {
 	private static final long serialVersionUID = 3250639793499599047L;
-	
+
 	private Card[] cards = new Card[3];
 
 	/**
-     * Constructor for the SetCardsAction class.
-     * 
-     * @param player  the player making the move
-     * @param c1 the first card chosen
-     * @param c2 the second card chosen
-     * @param c3 the third card chosen
-     */
-    public SetCardsAction(GamePlayer player, Card c1, Card c2, Card c3)
-    {
-        // Initialize the player with the superclass constructor
-        super(player);
-        
-        // Add cards to array
-        cards[0] = c1;
-        cards[1] = c2;
-        cards[2] = c3;
-    }
+	 * Constructor for the SetCardsAction class.
+	 * 
+	 * @param player  the player making the move
+	 * @param c1 the first card chosen
+	 * @param c2 the second card chosen
+	 * @param c3 the third card chosen
+	 */
+	public SetCardsAction(GamePlayer player, Card c1, Card c2, Card c3)
+	{
+		// Initialize the player with the superclass constructor
+		super(player);
 
-    /**
-     * @return whether this action is a card 
-     */
-    public boolean isCards() {
-        return true;
-    }
-    
-    /**
-     * @return whether this action is quit
-     */
-    public boolean isQuit() {
-        return false;
-    }
+		// Add cards to array
+		cards[0] = c1;
+		cards[1] = c2;
+		cards[2] = c3;
+	}
+
+	public Card getCard(int c) {
+		if (c >= 0 && c <= 2) {
+			return cards[c];
+		}
+		else return null;
+	}
+
+	/**
+	 * @return whether this action is a card 
+	 */
+	public boolean isCards() {
+		return true;
+	}
+
+	/**
+	 * @return whether this action is quit
+	 */
+	public boolean isQuit() {
+		return false;
+	}
 }
